21c82e57ad8fa424dd86cc4cdd3aed73eb72bd25
Chris <chlds@yahoo.com>
2019-10-29 19:59:05 +0900

    Learn

---
+++ c.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from lib import d,t
+
+
+def main():
+
+	r = t.t()
+	r = (r+(' '+(d.d())))
+	print(f'{r} :)')
+
+	return(0x01)
+
+
+if(__name__==('__main__')): main()
---
+++ lib/d.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+def d():
+
+	month = ['','January','February','March','April','May','Jane','July','August','September','October','November','December']
+	week = ['Mon.','Tue.','Wed.','Thu.','Fri.','Sat.','Sun.']
+
+	r = datetime.now()
+	w = r.weekday()
+
+	s = (str(week[w]))
+	s = (s+(' '+(str(r.day))))
+	s = (s+(' '+(month[r.month])))
+	s = (s+(' '+(str(r.year))))
+
+	if(__name__==('__main__')):
+
+		print(s)
+		return(0x01)
+
+	return(s)
+
+
+if(__name__==('__main__')): d()
